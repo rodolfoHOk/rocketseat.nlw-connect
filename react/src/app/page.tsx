4266feb20d3e7d22860574d1d72e57af2b23028f
Rodolfo HiOk <rodolfo.okino@gmail.com>
2025-02-17 21:11:13 -0300

    feat: react: create button, icon-button and input components

--- react/src/app/page.tsx
+++ react/src/app/page.tsx
@@ -1,9 +1,35 @@
+import { ArrowRight, Copy, Mail } from 'lucide-react';
 import { Button } from '@/components/button';
+import { IconButton } from '@/components/icon-button';
+import { InputRoot, InputIcon, InputField } from '@/components/input';
 
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <Button>
+        teste
+        <ArrowRight />
+      </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
+
+      <InputRoot>
+        <InputIcon>
+          <Mail />
+        </InputIcon>
+
+        <InputField placeholder="Digite seu e-mail" />
+      </InputRoot>
+
+      <InputRoot error>
+        <InputIcon>
+          <Mail />
+        </InputIcon>
+
+        <InputField placeholder="Digite seu e-mail" />
+      </InputRoot>
     </main>
   );
 }
--- react/src/components/button.tsx
+++ react/src/components/button.tsx
@@ -1,7 +1,12 @@
-interface ButtonProps {
-  text?: string;
-}
+import { ComponentProps } from 'react';
 
-export function Button({ text }: ButtonProps) {
-  return <button className="">{text}</button>;
+interface ButtonProps extends ComponentProps<'button'> {}
+
+export function Button(props: ButtonProps) {
+  return (
+    <button
+      className="w-full px-5 h-12 bg-gray-500 flex justify-between items-center text-blue font-semibold rounded-xl cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"
+      {...props}
+    />
+  );
 }
