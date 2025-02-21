00bbc588f97bb53144328507470c6fb1861ecfef
Rodolfo HiOk <rodolfo.okino@gmail.com>
2025-02-20 21:49:32 -0300

    feat: nodejs: add access invite link endpoint

--- nodejs/src/env.ts
+++ nodejs/src/env.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string(),
   REDIS_URL: z.string().url(),
+  WEB_URL: z.string().url(),
 })
 
 export const env = envSchema.parse(process.env)
